c74f7fee50912b858ae2764e88d5722eba4bf764
lloyd aleister bonds <lloydabonds@gmail.com>
2025-11-20 20:03:13 -0500

    got services layer running

---
+++ cli/cmd/health.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"cli/services"
+	"github.com/spf13/cobra"
+)
+
+var healthCmd = &cobra.Command{
+	Use:   "health",
+	Short: "tests health and connectivity of auto-doc ai agent backends",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		services.ShowIntegrated()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(healthCmd)
+}
---
+++ cli/services/healthservice.go
@@ -0,0 +1,10 @@
+package services
+
+import (
+	"fmt"
+)
+
+func ShowIntegrated() {
+	fmt.Println("initializing auto-doc...")
+	//code that calls the fastapi api
+}
