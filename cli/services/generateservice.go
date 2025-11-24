a12f786a664c9d34c3b8dba6db6632a9fde37bda
lloyd aleister bonds <lloydabonds@gmail.com>
2025-11-23 22:25:25 -0500

    moving onto go language server service

--- cli/services/generateservice.go
+++ cli/services/generateservice.go
@@ -17,18 +17,18 @@ func GenerateService() {
 	}
 
 	ignoredFiles := utils.GetIgnoredFiles(config)
-	description := utils.GetDescription(config)
+	// description := utils.GetDescription(config)
 	node := utils.BuildTree(cwd, name, ignoredFiles, "d")
 	treeAsJSON := utils.ConvertTreeToJSON(node)
 
 	goFiles, pyFiles := utils.DetectLanguages(node)
 
-	fmt.Println("========================================================================")
-	fmt.Println(description) //just to stop unused warning
-	fmt.Println("========================================================================")
+	//fmt.Println("========================================================================")
+	//fmt.Println(description) //just to stop unused warning
+	//fmt.Println("========================================================================")
 	fmt.Println(treeAsJSON)
 	fmt.Println("========================================================================")
 	fmt.Println(goFiles)
 	fmt.Println(pyFiles)
-	fmt.Println("==========================================================================")
+	//fmt.Println("==========================================================================")
 }
--- cli/services/utils/GetJSONData.go
+++ cli/services/utils/GetJSONData.go
@@ -8,6 +8,8 @@ import (
 	"os"
 )
 
+// GETS JSON DATA FROM AUTODOC CONFIG DOT JSON AND RETURNS A STRUCT WITH THESE OPTIONS
+// UNRELATED TO CONVERT TREE TO JSON
 func GetJSONData(d string) (models.Config, error) {
 	var cfgStruct = models.Config{}
 	entries, err := os.ReadDir(d)
