e8c737ef8f595a4f823792b6d9a62c8f2ad51a85
lloyd aleister bonds <lloydabonds@gmail.com>
2025-11-25 19:46:47 -0500

    working on agents layer

--- backend/app/agents/first_agent/agent.py
+++ backend/app/agents/first_agent/agent.py
@@ -1,8 +1,65 @@
-from google.adk.agents.llm_agent import Agent
+from google.adk.agents.llm_agent import SequentialAgent, ParallelAgent, LLMAgent
 
-root_agent = Agent(
-    model='<FILL_IN_MODEL>',
+model = "gemini-2.5-flash-lite"
+
+introduction_agent = LLMAgent(
+    model=model,
+    name="introduction_agent",
+    description="""given information about a project, generate, in markdown language ONLY, an introduction to a professional production README.md file 
+        the intro should cover the project name and project description. Answer questions like "what is this project, what does it do, who made it, why?" 
+        your output is put in the {intro_draft}. Be sure to create appropriate markdown elements that fit a production README. 
+        """,
+    output_key="intro_draft"
+)
+
+utility_agent = LLMAgent(
+    model=model,
+    name="utility_agent",
+    description="given information about a project, generate, in markdown language, "
+)
+
+parallel_documentation_team = ParallelAgent(
+    name="parallel_doc_team",
+    sub_agents=[introduction_agent, utility_agent, funcdoc_agent]
+)
+
+root_agent = SequentialAgent(
+    model=model,
     name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    description="the first reciever of a data object of class AgentContext",
+    instruction="""you will receive  payload with info about a software module.
+    The payload has four main parts:
+        1. n – The name of the module or application.
+        Example: "Autodoc CLI"
+        2. d – A short description of the module.
+        Example: "the CLI layer for a CLI application that auto-documents code"
+        3. t – The file tree for the module. Each node has:
+            n: the name of the file or directory
+            t: the type (d for directory, f for file)
+            c: a list of children if the node is a directory
+            Example:
+            {
+                "n": "cmd",
+                "t": "d",
+                "c": [
+                    {"n": "generate.go", "t": "f"},
+                    {"n": "health.go", "t": "f"}
+                ]
+            }
+        4. a – Function metadata for each file. Each entry includes:
+            f: the file name
+            s: a list of functions in that file. Each function has:
+            n: function name
+            p: parameters (may be null)
+            r: return values (may be null)
+            Example:
+            {
+                "f": "generate.go",
+                "s": [
+                    {"n": "init", "p": null, "r": null}
+                ]
+            }
+        Your goal is to run each subagent with appropriate context
+            """,
+    sub_agents=[parallel_documentation_team, judger_agent]
 )
--- backend/app/services/generateservice.py
+++ backend/app/services/generateservice.py
@@ -1,7 +1,16 @@
 from ..models.AgentContext import AgentContext
+from google.adk.runners import Runner
+from ..agents.first_agent import root_agent
 
 
 def generate(params: AgentContext):
-    # send to agent?
-    print(params.n)
-    print(params.d)
+
+    prompt = f"""
+    programName : {params.n},
+    programDescription: {params.d},
+    programFileTree: {params.t},
+    programFunctionSignatures: {params.a}
+    """
+    runner = Runner(agent=root_agent)
+    response = runner.run(prompt)
+    print(response)
